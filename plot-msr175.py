f4e461c499cfeecd0dbb375aaa3b6e78b0d4de5e
Takashi Nakamoto <takashi.nakamoto@nao.ac.jp>
2022-07-22 23:29:57 -0700

    Plot power spectrum.

--- plot-msr175.py
+++ plot-msr175.py
@@ -17,8 +17,8 @@ def decompose_msr175_data(msr175_data):
     z_g  = msr175_data[:, 3]
     return t_ms, x_g, y_g, z_g
 
-def plot_time_series(msr175_data,
-                     output_file,
+def plot_time_series(ax,
+                     msr175_data,
                      show_total = True,
                      show_max   = True,
                      acc_min_g  = float('nan'),
@@ -28,9 +28,6 @@ def plot_time_series(msr175_data,
     
     t_ms, x_g, y_g, z_g = decompose_msr175_data(msr175_data)
     
-    fig = plt.figure(str(output_file))
-    ax  = fig.add_subplot()
-
     ax.plot(t_ms, x_g, label = 'X')
     ax.plot(t_ms, y_g, label = 'Y')
     ax.plot(t_ms, z_g, label = 'Z')
@@ -68,12 +65,45 @@ def plot_time_series(msr175_data,
                 bbox = dict(facecolor = 'white',
                             edgecolor = 'black',
                             boxstyle  = 'round'))
-        
-    fig.savefig(output_file)
 
 def get_sampling_period_in_milliseconds(msr175_data):
     t_ms, x_g, y_g, z_g = decompose_msr175_data(msr175_data)
     return t_ms[1] - t_ms[0]
+
+def plot_power_spectrum(ax,
+                        msr175_data):
+    t_ms, x_g, y_g, z_g = decompose_msr175_data(msr175_data)
+
+    dt_ms = get_sampling_period_in_milliseconds(msr175_data)
+
+    x_ps = np.abs(np.fft.fft(x_g))**2
+    y_ps = np.abs(np.fft.fft(y_g))**2
+    z_ps = np.abs(np.fft.fft(z_g))**2
+
+    assert len(x_g) == len(y_g)
+    assert len(y_g) == len(z_g)
+    n = len(x_g)
+
+    freq_Hz = np.fft.fftfreq(n, dt_ms / 1000.0)
+
+    x_ps = x_ps[0:int(n/2)]
+    y_ps = y_ps[0:int(n/2)]
+    z_ps = z_ps[0:int(n/2)]
+    freq_Hz = freq_Hz[0:int(n/2)]
+    
+    ax.plot(freq_Hz, x_ps, label = 'X')
+    ax.plot(freq_Hz, y_ps, label = 'Y')
+    ax.plot(freq_Hz, z_ps, label = 'Z')
+    
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_ylabel(r'Power Spectrum [${\rm g}^2$]')
+    ax.legend(loc = 'upper right')
+    
+    # Set X axis range.
+    ax.set_xlim((0, freq_Hz[-1]))
+
+    # Set Y axis.
+    ax.set_yscale('log')
 
 def read_msr175_csv(csv_file_path):
     msr175_data = np.loadtxt(csv_file_path,
@@ -94,6 +124,11 @@ def parse_arguments():
                         default = 'png',
                         metavar = 'EXT',
                         help    = 'Extension of the image files for plots.')
+    parser.add_argument('--dpi',
+                        dest    = 'dpi',
+                        type    = float,
+                        default = 96.0,
+                        help    = 'DPI value for plot rendering.')
     parser.add_argument('--hide-total',
                         dest    = 'hide_total',
                         action  = 'store_true',
@@ -105,7 +140,7 @@ def parse_arguments():
     parser.add_argument('--show-plots',
                         dest    = 'show_plots',
                         action  = 'store_true',
-                        help    = 'Show the generated plots in GUI')
+                        help    = 'Show the generated plots in GUI.')
     parser.add_argument('--min-acc',
                         dest    = 'acc_min_g',
                         type    = float,
@@ -126,6 +161,10 @@ def parse_arguments():
                         type     = float,
                         default  = float('nan'),
                         help     = 'Maximum time in the time series plot in milliseconds. Specify "nan" for auto scale.')
+    parser.add_argument('--plot-power-spectrum',
+                        dest    = 'plot_power_spectrum',
+                        action  = 'store_true',
+                        help    = 'Plow power spectrum.')
     
     parser.add_argument('csv_file', nargs='+')
     
@@ -144,17 +183,30 @@ def main():
         msr175_data   = read_msr175_csv(csv_path)
         print(f'Loaded data from {csv_path}')
 
-        time_series_plot_path = directory.joinpath(f'{filename_base}.{args.plot_format}')
+        plot_path = directory.joinpath(f'{filename_base}.{args.plot_format}')
+        fig = plt.figure(str(plot_path))
         
-        plot_time_series(msr175_data,
-                         time_series_plot_path,
+        if args.plot_power_spectrum:
+            ax_time_series  = fig.add_subplot(2, 1, 1)
+        else:
+            ax_time_series  = fig.add_subplot(1, 1, 1)
+        
+        plot_time_series(ax_time_series,
+                         msr175_data,
                          show_total = not args.hide_total,
                          show_max   = not args.hide_max,
                          acc_min_g  = args.acc_min_g,
                          acc_max_g  = args.acc_max_g,
                          t_min_ms   = args.t_min_ms,
                          t_max_ms   = args.t_max_ms)
-        print(f'Generated time series plot as {time_series_plot_path}')
+
+        if args.plot_power_spectrum:
+            ax_power_spectrum = fig.add_subplot(2, 1, 2)
+            plot_power_spectrum(ax_power_spectrum,
+                                msr175_data)
+
+        fig.savefig(plot_path, dpi = args.dpi)
+        print(f'Generated the plot as {plot_path}')
 
     if args.show_plots:
         plt.show()
